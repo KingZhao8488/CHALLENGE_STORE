196f457cd0feaf017f9fc4887edaa3b57649b3b6
KingZhao8488 <lordarcane8407@outlook.com>
2025-04-19 11:16:04 -0500

    planteamiento de algoritmo inicial de analisis de ingreso total por tienda

--- challenge.py
+++ challenge.py
@@ -12,3 +12,18 @@ tienda3 = pd.read_csv(url3)
 tienda4 = pd.read_csv(url4)
 
 tienda.head()
+
+# calcular el ingreso total de cada tienda. Sumando los valores de la columna Precio de cada conjunto de datos de la tienda para estimar los ingresos.
+ingresos_tienda1 = tienda['Precio'].sum()
+ingresos_tienda2 = tienda2['Precio'].sum()
+ingresos_tienda3 = tienda3['Precio'].sum()
+ingresos_tienda4 = tienda4['Precio'].sum()
+
+# Crear un DataFrame con los ingresos de cada tienda
+ingresos = pd.DataFrame({
+    'Tienda': ['Tienda 1', 'Tienda 2', 'Tienda 3', 'Tienda 4'],
+    'Ingreso': [ingresos_tienda1, ingresos_tienda2, ingresos_tienda3, ingresos_tienda4]
+})
+
+# Mostrar el DataFrame de ingresos
+print(ingresos)
